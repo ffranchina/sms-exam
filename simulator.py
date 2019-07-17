11b798b13485f7eebbbd05c5fd4d76d5931dca44
Francesco Franchina <cescus92@gmail.com>
2019-07-17 16:39:36 +0200

    Initial skeleton of the project

---
+++ simulator.py
@@ -0,0 +1,17 @@
+
+class Runner:
+    def __init__(self):
+        pass
+
+class Population:
+
+    def __init__(self):
+        pass
+
+class Actor:
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
